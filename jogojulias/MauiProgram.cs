4697e2831b1a98c6dc73ad0bd131a8d089bed35a
julias <jsantana23julia@gmail.com>
2024-03-04 11:51:26 -0300

    teste

--- jogojulias/MauiProgram.cs
+++ jogojulias/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("SmileNice.otf", "SmileNices");
 			});
 
 #if DEBUG
